7b705d25b140469cdd4b3adc718cd65eb44f2f20
Rocky <79947006+RockyRosso@users.noreply.github.com>
2025-02-17 13:51:34 -0500

    Added modern compiler for sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,6 +49,16 @@ export default defineNuxtConfig({
         },
     },
 
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    api: "modern-compiler",
+                },
+            },
+        },
+    },
+
     build: {
         transpile: [
             'global',
